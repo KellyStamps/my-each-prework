af5a9a5a9d15c9e9e7a8451dcac0ff12d1038a6f
Kelly S <kellydsample@gmail.com>
2017-10-13 22:48:30 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+
+def my_each (array)# put argument(s) here
   # code here
+  array = array
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i+1
+  end
+  array
+end
+arr = ["yes","no","maybe"]
+my_each (arr) do |i|
+    i
 end
